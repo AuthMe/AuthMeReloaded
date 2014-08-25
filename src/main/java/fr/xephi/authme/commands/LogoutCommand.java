655a6a1a71fe39e7d6450dccb06be439513d3db2
Xephi <xephirot59@gmail.com>
2014-08-25 04:34:06 +0200

    Correctly de-op on /logout

--- src/main/java/fr/xephi/authme/commands/LogoutCommand.java
+++ src/main/java/fr/xephi/authme/commands/LogoutCommand.java
@@ -110,6 +110,9 @@ public class LogoutCommand implements CommandExecutor {
         }
         if (Settings.applyBlindEffect)
             player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Settings.getRegistrationTimeout * 20, 2));
+        player.setOp(false);
+        player.setFlying(true);
+        player.setAllowFlight(true);
         m._(player, "logout");
         ConsoleLogger.info(player.getDisplayName() + " logged out");
         if (plugin.notifications != null) {
