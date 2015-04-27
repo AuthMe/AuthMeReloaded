a80792d9479070df399f95b44a10c46fb4bb3711
Xephi <xephirot59@gmail.com>
2015-04-27 12:52:06 +0200

    LowerCase expected

--- src/main/java/fr/xephi/authme/cache/auth/PlayerCache.java
+++ src/main/java/fr/xephi/authme/cache/auth/PlayerCache.java
@@ -12,24 +12,24 @@ public class PlayerCache {
     }
 
     public void addPlayer(PlayerAuth auth) {
-        cache.put(auth.getNickname(), auth);
+        cache.put(auth.getNickname().toLowerCase(), auth);
     }
 
     public void updatePlayer(PlayerAuth auth) {
-        cache.remove(auth.getNickname());
-        cache.put(auth.getNickname(), auth);
+        cache.remove(auth.getNickname().toLowerCase());
+        cache.put(auth.getNickname().toLowerCase(), auth);
     }
 
     public void removePlayer(String user) {
-        cache.remove(user);
+        cache.remove(user.toLowerCase());
     }
 
     public boolean isAuthenticated(String user) {
-        return cache.containsKey(user);
+        return cache.containsKey(user.toLowerCase());
     }
 
     public PlayerAuth getAuth(String user) {
-        return cache.get(user);
+        return cache.get(user.toLowerCase());
     }
 
     public static PlayerCache getInstance() {
