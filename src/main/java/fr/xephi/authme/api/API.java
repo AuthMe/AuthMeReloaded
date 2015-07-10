f3f4a2309813b25c2eb37ddcfc5c5dd68d82f7a0
Maxetto <max-8000@hotmail.it>
2015-07-10 16:32:26 +0200

    Sometimes I'm sleeping

--- src/main/java/fr/xephi/authme/api/API.java
+++ src/main/java/fr/xephi/authme/api/API.java
@@ -153,7 +153,7 @@ public class API {
             if (isRegistered(name)) {
                 return false;
             }
-            PlayerAuth auth = new PlayerAuth(name, hash, "198.168.0.1", 0, "your@email.com");
+            PlayerAuth auth = new PlayerAuth(name, hash, "192.168.0.1", 0, "your@email.com");
             if (!database.saveAuth(auth)) {
                 return false;
             }
