38f4cb0231a3706621ae0e01ed7dd7567f2c26a5
Xephi59 <xephirot59@gmail.com>
2015-07-22 21:47:08 +0200

    Push a model for module type - DEV

---
+++ src/main/java/fr/xephi/authme/modules/Module.java
@@ -0,0 +1,27 @@
+package fr.xephi.authme.modules;
+
+import fr.xephi.authme.AuthMe;
+
+public interface Module {
+
+    public String getName();
+
+    public AuthMe getInstanceOfAuthMe();
+
+    public Module getInstance();
+
+    public enum ModuleType {
+        MANAGER,
+        MYSQL,
+        ACTIONS,
+        CONVERTERS,
+        EMAILS,
+        CUSTOM;
+    }
+
+    public ModuleType getType();
+
+    public boolean load();
+
+    public boolean unload();
+}
