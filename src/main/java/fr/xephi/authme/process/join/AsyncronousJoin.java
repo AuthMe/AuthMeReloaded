fa7c8be10f2f92dce645274e6327b10589d26a31
Xephi59 <xephirot59@gmail.com>
2015-08-02 00:40:00 +0200

    Try to fix lowercase issue

--- src/main/java/fr/xephi/authme/process/join/AsyncronousJoin.java
+++ src/main/java/fr/xephi/authme/process/join/AsyncronousJoin.java
@@ -101,7 +101,7 @@ public class AsyncronousJoin {
         final Location spawnLoc = plugin.getSpawnLocation(player);
         if (database.isAuthAvailable(name)) {
             PlayerAuth auth = database.getAuth(name);
-            if (auth.getRealName() != null && !auth.getRealName().equals("Player") && !auth.getRealName().equals(player.getName())) {
+            if (auth.getRealName() != null && !auth.getRealName().isEmpty() && !auth.getRealName().equalsIgnoreCase("Player") && !auth.getRealName().equals(player.getName())) {
                 final GameMode gM = AuthMePlayerListener.gameMode.get(name);
                 sched.scheduleSyncDelayedTask(plugin, new Runnable() {
 
