5e991f7f420bc53244e1588a5d6eeb91f4b926c3
vanhec_a <alexandre.vanhecke@epitech.eu>
2015-10-12 15:12:01 +0200

    realName here

--- src/main/java/fr/xephi/authme/commands/AdminCommand.java
+++ src/main/java/fr/xephi/authme/commands/AdminCommand.java
@@ -262,7 +262,7 @@ public class AdminCommand implements CommandExecutor {
                             return;
                         }
                         String hash = PasswordSecurity.getHash(Settings.getPasswordHash, lowpass, name);
-                        PlayerAuth auth = new PlayerAuth(name, hash, "192.168.0.1", 0L, "your@email.com", name);
+                        PlayerAuth auth = new PlayerAuth(name, hash, "192.168.0.1", 0L, "your@email.com", realName);
                         if (PasswordSecurity.userSalt.containsKey(name) && PasswordSecurity.userSalt.get(name) != null)
                             auth.setSalt(PasswordSecurity.userSalt.get(name));
                         else auth.setSalt("");
