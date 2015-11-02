6d4682793620eab0e8d744305e743c79ab18b075
Tim Visée <timvisee@gmail.com>
2015-11-02 20:20:59 +0100

    Removed unused TODO

--- src/main/java/fr/xephi/authme/command/executable/authme/AuthMeCommand.java
+++ src/main/java/fr/xephi/authme/command/executable/authme/AuthMeCommand.java
@@ -21,7 +21,6 @@ public class AuthMeCommand extends ExecutableCommand {
     @Override
     public boolean executeCommand(CommandSender sender, CommandParts commandReference, CommandParts commandArguments) {
         // Show some version info
-        // TODO: Use plugin name and version constants here!
         sender.sendMessage(ChatColor.GREEN + "This server is running " + AuthMe.PLUGIN_NAME + " v" + AuthMe.getVersionName() + "! " + ChatColor.RED + "<3");
         sender.sendMessage(ChatColor.YELLOW + "Use the command " + ChatColor.GOLD + "/" + commandReference.get(0) + " help" + ChatColor.YELLOW + " to view help.");
         sender.sendMessage(ChatColor.YELLOW + "Use the command " + ChatColor.GOLD + "/" + commandReference.get(0) + " about" + ChatColor.YELLOW + " to view about.");
