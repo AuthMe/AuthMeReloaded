d3edbbcb49b67db8f8a21e19659db198142c9708
ljacqu <ljacqu@users.noreply.github.com>
2015-12-05 23:55:58 +0100

    #285 #319 Add support for wildcard permissions
    - Fix accidental recursive call

--- src/main/java/fr/xephi/authme/permission/PermissionsManager.java
+++ src/main/java/fr/xephi/authme/permission/PermissionsManager.java
@@ -315,7 +315,7 @@ public class PermissionsManager implements PermissionsService {
 
     public boolean hasPermission(Player player, PermissionNode permissionNode, boolean def) {
         return hasPermission(player, permissionNode.getNode(), def)
-            || hasPermission(player, permissionNode.getWildcardNode(), def);
+            || hasPermission(player, permissionNode.getWildcardNode().getNode(), def);
     }
 
     public boolean hasPermission(Player player, Iterable<PermissionNode> nodes, boolean def) {
