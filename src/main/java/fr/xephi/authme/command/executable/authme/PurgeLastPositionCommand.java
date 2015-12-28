c94b6223a110b016acae4e30ddd3617716894cb0
Xephi <xephirot59@gmail.com>
2015-12-28 11:50:16 +0100

    Add wildcard to perform reset position for each players

--- src/main/java/fr/xephi/authme/command/executable/authme/PurgeLastPositionCommand.java
+++ src/main/java/fr/xephi/authme/command/executable/authme/PurgeLastPositionCommand.java
@@ -15,21 +15,38 @@ public class PurgeLastPositionCommand implements ExecutableCommand {
         String playerName = arguments.isEmpty() ? sender.getName() : arguments.get(0);
         String playerNameLowerCase = playerName.toLowerCase();
 
-        // Get the user auth and make sure the user exists
-        PlayerAuth auth = commandService.getDataSource().getAuth(playerNameLowerCase);
-        if (auth == null) {
-            commandService.send(sender, MessageKey.UNKNOWN_USER);
-            return;
+        if (playerNameLowerCase.equalsIgnoreCase("*"))
+        {
+        	for (PlayerAuth auth : commandService.getDataSource().getAllAuths())
+        	{
+                // Set the last position
+                auth.setQuitLocX(0D);
+                auth.setQuitLocY(0D);
+                auth.setQuitLocZ(0D);
+                auth.setWorld("world");
+                commandService.getDataSource().updateQuitLoc(auth);
+        	}
+        	sender.sendMessage("All players last position locations are now reset");
+        }
+        else
+        {
+            // Get the user auth and make sure the user exists
+            PlayerAuth auth = commandService.getDataSource().getAuth(playerNameLowerCase);
+            if (auth == null) {
+                commandService.send(sender, MessageKey.UNKNOWN_USER);
+                return;
+            }
+
+            // Set the last position
+            auth.setQuitLocX(0D);
+            auth.setQuitLocY(0D);
+            auth.setQuitLocZ(0D);
+            auth.setWorld("world");
+            commandService.getDataSource().updateQuitLoc(auth);
+
+            // Show a status message
+            sender.sendMessage(playerNameLowerCase + "'s last position location is now reset");
         }
 
-        // Set the last position
-        auth.setQuitLocX(0D);
-        auth.setQuitLocY(0D);
-        auth.setQuitLocZ(0D);
-        auth.setWorld("world");
-        commandService.getDataSource().updateQuitLoc(auth);
-
-        // Show a status message
-        sender.sendMessage(playerNameLowerCase + "'s last position location is now reset");
     }
 }
