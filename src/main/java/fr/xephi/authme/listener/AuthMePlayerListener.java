fe31b5cabb5e1a3b4dc8a9e6cb859b6d6c1f2add
DNx5 <dani.narianto@gmail.com>
2016-01-03 09:38:30 +0700

    I forgot to remove the old check.

--- src/main/java/fr/xephi/authme/listener/AuthMePlayerListener.java
+++ src/main/java/fr/xephi/authme/listener/AuthMePlayerListener.java
@@ -206,10 +206,6 @@ public class AuthMePlayerListener implements Listener {
             joinMessage.put(name, joinMsg);
         }
 
-        if (Settings.checkVeryGames) {
-            plugin.getVerygamesIp(player);
-        }
-
         // Shedule login task so works after the prelogin
         // (Fix found by Koolaid5000)
         Bukkit.getScheduler().runTask(plugin, new Runnable() {
