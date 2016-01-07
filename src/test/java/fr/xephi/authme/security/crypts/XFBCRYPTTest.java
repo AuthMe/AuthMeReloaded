2c13783c6cbe298a2dd6e6c998b51cd9b6980064
DNx5 <dani.narianto@gmail.com>
2016-01-07 07:12:30 +0700

    Created test for XFBCRYPT.

--- src/test/java/fr/xephi/authme/security/crypts/XFBCRYPTTest.java
+++ src/test/java/fr/xephi/authme/security/crypts/XFBCRYPTTest.java
@@ -1,19 +1,17 @@
 package fr.xephi.authme.security.crypts;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * Test for {@link XFBCRYPT}.
  */
-@Ignore
-// TODO #137: Create a test class as for the other encryption methods. Simply run the following test and copy the
-// output -- that's your test class! (Once XFBCRYPT.java actually works properly)
-// @org.junit.Test public void a() { AbstractEncryptionMethodTest.generateTest(new XFBCRYPT()); }
-public class XFBCRYPTTest {
+public class XFBCRYPTTest extends AbstractEncryptionMethodTest {
 
-    @Test
-    public void shouldComputeHash() {
-        System.out.println(new XFBCRYPT().computeHash("Test", "name"));
+    public XFBCRYPTTest() {
+        super(new XFBCRYPT(),
+            "$2a$10$UtuON/ZG.x8EWG/zQbryB.BHfQVrfxk3H7qykzP.UJQ8YiLjZyfqq",  // password
+            "$2a$10$Q.ocUo.YtHTdI4nu3pcpKun6BILcmWHm541ANULucmuU/ps1QKY4K",  // PassWord1
+            "$2a$10$yHjm02.K4HP5iFU1F..yLeTeo7PWZVbKAr/QGex5jU4.J3mdq/uuO",  // &^%te$t?Pw@_
+            "$2a$10$joIayhGStExKWxNbiqMMPOYFSpQ76HVNjpOB7.QwTmG5q.TiJJ.0e"); // âË_3(íù*
     }
 }
