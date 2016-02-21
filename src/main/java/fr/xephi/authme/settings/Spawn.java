5e330398d390bcc3c939e24b1672ba8925c3b8ec
DNx5 <dani.narianto@gmail.com>
2016-02-22 01:01:05 +0700

    Spawn Location

--- src/main/java/fr/xephi/authme/settings/Spawn.java
+++ src/main/java/fr/xephi/authme/settings/Spawn.java
@@ -1,7 +1,14 @@
 package fr.xephi.authme.settings;
 
+import com.onarandombox.MultiverseCore.api.MVWorldManager;
+import fr.xephi.authme.AuthMe;
+import fr.xephi.authme.ConsoleLogger;
+import fr.xephi.authme.cache.auth.PlayerCache;
+import fr.xephi.authme.util.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 
@@ -12,13 +19,17 @@ import java.io.File;
 public class Spawn extends CustomConfiguration {
 
     private static Spawn spawn;
+    private static String[] spawnPriority;
 
-    public Spawn() {
-        super(new File("." + File.separator + "plugins" + File.separator + "AuthMe" + File.separator + "spawn.yml"));
-        spawn = this;
-        load();
-        save();
-        saveDefault();
+    private Spawn() {
+        super(new File(Settings.PLUGIN_FOLDER, "spawn.yml"));
+        reload();
+    }
+
+    public static void reload() {
+        getInstance().load();
+        getInstance().save();
+        spawnPriority = Settings.spawnPriority.split(",");
     }
 
     /**
@@ -33,111 +44,109 @@ public class Spawn extends CustomConfiguration {
         return spawn;
     }
 
-    private void saveDefault() {
-        if (!contains("spawn")) {
-            set("spawn.world", "");
-            set("spawn.x", "");
-            set("spawn.y", "");
-            set("spawn.z", "");
-            set("spawn.yaw", "");
-            set("spawn.pitch", "");
-            save();
-        }
-        if (!contains("firstspawn")) {
-            set("firstspawn.world", "");
-            set("firstspawn.x", "");
-            set("firstspawn.y", "");
-            set("firstspawn.z", "");
-            set("firstspawn.yaw", "");
-            set("firstspawn.pitch", "");
-            save();
-        }
-    }
-
-    /**
-     * Method setSpawn.
-     *
-     * @param location Location
-     *
-     * @return boolean
-     */
     public boolean setSpawn(Location location) {
-        try {
-            set("spawn.world", location.getWorld().getName());
-            set("spawn.x", location.getX());
-            set("spawn.y", location.getY());
-            set("spawn.z", location.getZ());
-            set("spawn.yaw", location.getYaw());
-            set("spawn.pitch", location.getPitch());
-            save();
-            return true;
-        } catch (NullPointerException npe) {
+        if (location == null || location.getWorld() == null) {
             return false;
         }
+        set("spawn.world", location.getWorld().getName());
+        set("spawn.x", location.getX());
+        set("spawn.y", location.getY());
+        set("spawn.z", location.getZ());
+        set("spawn.yaw", location.getYaw());
+        set("spawn.pitch", location.getPitch());
+        save();
+        return true;
     }
 
-    /**
-     * Method setFirstSpawn.
-     *
-     * @param location Location
-     *
-     * @return boolean
-     */
     public boolean setFirstSpawn(Location location) {
-        try {
-            set("firstspawn.world", location.getWorld().getName());
-            set("firstspawn.x", location.getX());
-            set("firstspawn.y", location.getY());
-            set("firstspawn.z", location.getZ());
-            set("firstspawn.yaw", location.getYaw());
-            set("firstspawn.pitch", location.getPitch());
-            save();
-            return true;
-        } catch (NullPointerException npe) {
+        if (location == null || location.getWorld() == null) {
             return false;
         }
+        set("firstspawn.world", location.getWorld().getName());
+        set("firstspawn.x", location.getX());
+        set("firstspawn.y", location.getY());
+        set("firstspawn.z", location.getZ());
+        set("firstspawn.yaw", location.getYaw());
+        set("firstspawn.pitch", location.getPitch());
+        save();
+        return true;
     }
 
-    /**
-     * Method getLocation.
-     *
-     * @return Location
-     */
-    @Deprecated
-    public Location getLocation() {
-        return getSpawn();
-    }
-
-    /**
-     * Method getSpawn.
-     *
-     * @return Location
-     */
     public Location getSpawn() {
         try {
-            if (this.getString("spawn.world").isEmpty() || this.getString("spawn.world").equals(""))
+            String worldName;
+            World world;
+            if (StringUtils.isEmpty(worldName = getString("spawn.world")) ||
+                (world = Bukkit.getWorld(worldName)) == null) {
                 return null;
-            Location location = new Location(Bukkit.getWorld(this.getString("spawn.world")), this.getDouble("spawn.x"), this.getDouble("spawn.y"), this.getDouble("spawn.z"), Float.parseFloat(this.getString("spawn.yaw")), Float.parseFloat(this.getString("spawn.pitch")));
-            return location;
-        } catch (NullPointerException | NumberFormatException npe) {
+            }
+            return new Location(world, getDouble("spawn.x"), getDouble("spawn.y"), getDouble("spawn.z"),
+                Float.parseFloat(getString("spawn.yaw")), Float.parseFloat(getString("spawn.pitch")));
+        } catch (NumberFormatException e) {
+            ConsoleLogger.writeStackTrace(e);
             return null;
         }
     }
 
-    /**
-     * Method getFirstSpawn.
-     *
-     * @return Location
-     */
     public Location getFirstSpawn() {
         try {
-            if (this.getString("firstspawn.world").isEmpty() || this.getString("firstspawn.world").equals(""))
+            String worldName;
+            World world;
+            if (StringUtils.isEmpty(worldName = getString("firstspawn.world")) ||
+                (world = Bukkit.getWorld(worldName)) == null) {
                 return null;
-            Location location = new Location(Bukkit.getWorld(this.getString("firstspawn.world")), this.getDouble("firstspawn.x"), this.getDouble("firstspawn.y"), this.getDouble("firstspawn.z"), Float.parseFloat(this.getString("firstspawn.yaw")), Float.parseFloat(this.getString("firstspawn.pitch")));
-            return location;
-        } catch (NullPointerException | NumberFormatException npe) {
+            }
+            return new Location(world, getDouble("firstspawn.x"), getDouble("firstspawn.y"), getDouble("firstspawn.z"),
+                Float.parseFloat(getString("firstspawn.yaw")), Float.parseFloat(getString("firstspawn.pitch")));
+        } catch (NumberFormatException e) {
+            ConsoleLogger.writeStackTrace(e);
             return null;
         }
     }
 
+    // Return the spawn location of a player
+    public Location getSpawnLocation(Player player) {
+        AuthMe plugin = AuthMe.getInstance();
+        World world;
+        if (plugin == null || player == null || (world = player.getWorld()) == null) {
+            return null;
+        }
+
+        Location spawnLoc = null;
+        for (String priority : spawnPriority) {
+            switch (priority.toLowerCase()) {
+                case "default":
+                    if (world.getSpawnLocation() != null) {
+                        spawnLoc = world.getSpawnLocation();
+                    }
+                    break;
+                case "multiverse":
+                    if (Settings.multiverse && plugin.multiverse != null) {
+                        MVWorldManager manager = plugin.multiverse.getMVWorldManager();
+                        if (manager.isMVWorld(world)) {
+                            spawnLoc = manager.getMVWorld(world).getSpawnLocation();
+                        }
+                    }
+                    break;
+                case "essentials":
+                    spawnLoc = plugin.essentialsSpawn;
+                    break;
+                case "authme":
+                    String playerNameLower = player.getName().toLowerCase();
+                    if (PlayerCache.getInstance().isAuthenticated(playerNameLower)) {
+                        spawnLoc = getSpawn();
+                    } else if ((getFirstSpawn() != null) && (!player.hasPlayedBefore() ||
+                        (!plugin.getDataSource().isAuthAvailable(playerNameLower)))) {
+                        spawnLoc = getFirstSpawn();
+                    } else {
+                        spawnLoc = getSpawn();
+                    }
+                    break;
+            }
+            if (spawnLoc != null) {
+                return spawnLoc;
+            }
+        }
+        return world.getSpawnLocation(); // return default location
+    }
 }
