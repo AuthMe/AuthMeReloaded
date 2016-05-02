e97b4641a1780c2cadd1101d4f24f42c9cac5a8c
Xephi59 <xephirot59@gmail.com>
2016-05-02 02:39:53 +0200

    Kick player only Synchronously

--- src/main/java/fr/xephi/authme/command/executable/authme/RegisterAdminCommand.java
+++ src/main/java/fr/xephi/authme/command/executable/authme/RegisterAdminCommand.java
@@ -1,15 +1,17 @@
 package fr.xephi.authme.command.executable.authme;
 
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import fr.xephi.authme.AuthMe;
 import fr.xephi.authme.ConsoleLogger;
 import fr.xephi.authme.cache.auth.PlayerAuth;
 import fr.xephi.authme.command.CommandService;
 import fr.xephi.authme.command.ExecutableCommand;
 import fr.xephi.authme.output.MessageKey;
 import fr.xephi.authme.security.crypts.HashedPassword;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import java.util.List;
 
 /**
  * Admin command to register a user.
@@ -55,9 +57,15 @@ public class RegisterAdminCommand implements ExecutableCommand {
 
                 commandService.send(sender, MessageKey.REGISTER_SUCCESS);
                 ConsoleLogger.info(sender.getName() + " registered " + playerName);
-                Player player = commandService.getPlayer(playerName);
+                final Player player = commandService.getPlayer(playerName);
                 if (player != null) {
-                    player.kickPlayer("An admin just registered you, please log in again");
+                    sender.getServer().getScheduler().scheduleSyncDelayedTask(AuthMe.getInstance(), new Runnable()
+                            {
+                                @Override
+                                public void run() {
+                                    player.kickPlayer("An admin just registered you, please log in again");
+                                }
+                            });
                 }
             }
         });
