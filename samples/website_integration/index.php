383820cd227b5d64238e5ea3e33c5790f48c0c30
ljacqu <ljacqu@users.noreply.github.com>
2016-05-19 21:44:24 +0200

    #702 Implement SHA256 php registration - Refactor Bcrypt and Sha256 examples to use common abstract parent class - Implement hashing logic for Sha256

--- samples/website_integration/index.php
+++ samples/website_integration/index.php
@@ -1,6 +1,6 @@
 <!--
-  This is a demo page for AuthMe website integration with BCrypt.
-  See integration.php for the PHP code you need.
+  This is a demo page for AuthMe website integration.
+  See AuthMeController.php and the extending classes for the PHP code you need.
 -->
 <!DOCTYPE html>
 <html lang="en">
@@ -12,17 +12,24 @@
 <?php
 error_reporting(E_ALL);
 
+require 'AuthMeController.php';
+
+// Change this to the file of the hash encryption you need, e.g. Bcrypt.php or Sha256.php
+require 'Sha256.php';
+// The class name must correspond to the file you have in require above! e.g. require 'Sha256.php'; and new Sha256();
+$authme_controller = new Sha256();
+
 $action = get_from_post_or_empty('action');
 $user = get_from_post_or_empty('username');
 $pass = get_from_post_or_empty('password');
 
 $was_successful = false;
 if ($action && $user && $pass) {
-    require_once('integration.php');
+    require_once('Bcrypt.php');
     if ($action === 'Log in') {
-        $was_successful = process_login($user, $pass);
+        $was_successful = process_login($user, $pass, $authme_controller);
     } else if ($action === 'Register') {
-        $was_successful = process_register($user, $pass);
+        $was_successful = process_register($user, $pass, $authme_controller);
     }
 }
 
@@ -50,11 +57,11 @@ function get_from_post_or_empty($index_name) {
 
 
 // Login logic
-function process_login($user, $pass) {
-    if (authme_check_password($user, $pass)) {
+function process_login($user, $pass, AuthMeController $controller) {
+    if ($controller->checkPassword($user, $pass)) {
         printf('<h1>Hello, %s!</h1>', htmlspecialchars($user));
         echo 'Successful login. Nice to have you back!'
-            . '<br /><a href="form.php">Back to form</a>';
+            . '<br /><a href="index.php">Back to form</a>';
         return true;
     } else {
         echo '<h1>Error</h1> Invalid username or password.';
@@ -63,15 +70,15 @@ function process_login($user, $pass) {
 }
 
 // Register logic
-function process_register($user, $pass) {
-    if (authme_has_user($user)) {
+function process_register($user, $pass, AuthMeController $controller) {
+    if ($controller->isUserRegistered($user)) {
         echo '<h1>Error</h1> This user already exists.';
     } else {
         // Note that we don't validate the password or username at all in this demo...
-        $register_success = authme_register($user, $pass);
+        $register_success = $controller->register($user, $pass);
         if ($register_success) {
             printf('<h1>Welcome, %s!</h1>Thanks for registering', htmlspecialchars($user));
-            echo '<br /><a href="form.php">Back to form</a>';
+            echo '<br /><a href="index.php">Back to form</a>';
             return true;
         } else {
             echo '<h1>Error</h1>Unfortunately, there was an error during the registration.';
