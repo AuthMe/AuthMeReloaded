09d6f5c92377ed2af91c0697cef6dd07a091f8e1
Gabriele C <sgdc3.mail@gmail.com>
2016-07-12 22:10:30 +0200

    Close #816

--- src/main/java/fr/xephi/authme/process/logout/AsynchronousLogout.java
+++ src/main/java/fr/xephi/authme/process/logout/AsynchronousLogout.java
@@ -1,5 +1,6 @@
 package fr.xephi.authme.process.logout;
 
+import fr.xephi.authme.cache.SessionManager;
 import fr.xephi.authme.cache.auth.PlayerAuth;
 import fr.xephi.authme.cache.auth.PlayerCache;
 import fr.xephi.authme.cache.limbo.LimboCache;
@@ -8,6 +9,7 @@ import fr.xephi.authme.output.MessageKey;
 import fr.xephi.authme.process.AsynchronousProcess;
 import fr.xephi.authme.process.ProcessService;
 import fr.xephi.authme.process.SyncProcessManager;
+import fr.xephi.authme.settings.properties.PluginSettings;
 import fr.xephi.authme.settings.properties.RestrictionSettings;
 import org.bukkit.entity.Player;
 
@@ -28,6 +30,9 @@ public class AsynchronousLogout implements AsynchronousProcess {
     private LimboCache limboCache;
 
     @Inject
+    private SessionManager sessionManager;
+
+    @Inject
     private SyncProcessManager syncProcessManager;
 
     AsynchronousLogout() {
@@ -45,6 +50,11 @@ public class AsynchronousLogout implements AsynchronousProcess {
         if (service.getProperty(RestrictionSettings.SAVE_QUIT_LOCATION)) {
             auth.setQuitLocation(player.getLocation());
             database.updateQuitLoc(auth);
+        }
+
+        if (service.getProperty(PluginSettings.SESSIONS_ENABLED) && (sessionManager.hasSession(name) || database.isLogged(name))) {
+            sessionManager.cancelSession(name);
+            service.send(player, MessageKey.SESSION_EXPIRED);
         }
 
         limboCache.addPlayerData(player);
