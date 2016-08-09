76f0178d0f4562705a5dd1181b7303cdd8e0d3df
Gabriele C <sgdc3.mail@gmail.com>
2016-08-09 14:53:50 +0200

    Whoops

--- src/main/java/fr/xephi/authme/process/join/AsynchronousJoin.java
+++ src/main/java/fr/xephi/authme/process/join/AsynchronousJoin.java
@@ -13,6 +13,7 @@ import fr.xephi.authme.output.MessageKey;
 import fr.xephi.authme.permission.AuthGroupType;
 import fr.xephi.authme.permission.PlayerStatePermission;
 import fr.xephi.authme.process.AsynchronousProcess;
+import fr.xephi.authme.process.Management;
 import fr.xephi.authme.process.ProcessService;
 import fr.xephi.authme.settings.properties.HooksSettings;
 import fr.xephi.authme.settings.properties.PluginSettings;
