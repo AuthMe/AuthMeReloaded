5ec1c3fe24cfec1ec73492fdd82bd2f58854f6b7
ljacqu <ljacqu@users.noreply.github.com>
2016-11-26 16:34:14 +0100

    #1018 Load group names when AuthGroupHandler is constructed

--- src/main/java/fr/xephi/authme/permission/AuthGroupHandler.java
+++ src/main/java/fr/xephi/authme/permission/AuthGroupHandler.java
@@ -10,6 +10,7 @@ import fr.xephi.authme.settings.properties.PluginSettings;
 import fr.xephi.authme.settings.properties.SecuritySettings;
 import org.bukkit.entity.Player;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.util.Arrays;
 
@@ -115,6 +116,7 @@ public class AuthGroupHandler implements Reloadable {
     }
 
     @Override
+    @PostConstruct
     public void reload() {
         unloggedInGroup = settings.getProperty(SecuritySettings.UNLOGGEDIN_GROUP);
         unregisteredGroup = settings.getProperty(HooksSettings.UNREGISTERED_GROUP);
