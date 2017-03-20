3c45ca8425b4d7669ad95cc9719db8fb8d7b52fb
ljacqu <ljacqu@users.noreply.github.com>
2017-03-20 20:57:09 +0100

    #1100 Check that auth exists for /email show - This case should never happen, but better to be safe

--- src/main/java/fr/xephi/authme/command/executable/email/ShowEmailCommand.java
+++ src/main/java/fr/xephi/authme/command/executable/email/ShowEmailCommand.java
@@ -24,7 +24,7 @@ public class ShowEmailCommand extends PlayerCommand {
     @Override
     public void runCommand(Player player, List<String> arguments) {
         PlayerAuth auth = playerCache.getAuth(player.getName());
-        if (auth.getEmail() != null && !"your@email.com".equalsIgnoreCase(auth.getEmail())) {
+        if (auth != null && auth.getEmail() != null && !"your@email.com".equalsIgnoreCase(auth.getEmail())) {
             commonService.send(player, MessageKey.EMAIL_SHOW, auth.getEmail());
         } else {
             commonService.send(player, MessageKey.SHOW_NO_EMAIL);
