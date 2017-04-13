0d395a2d5e05a64d865a510e89f320805c7c4aae
Gabriele C <sgdc3.mail@gmail.com>
2017-04-14 01:41:40 +0200

    Implement ARGON2 hash
    #1150

---
+++ src/main/java/fr/xephi/authme/security/crypts/Argon2.java
@@ -0,0 +1,20 @@
+package fr.xephi.authme.security.crypts;
+
+import de.mkammerer.argon2.Argon2Factory;
+import fr.xephi.authme.security.crypts.description.Recommendation;
+import fr.xephi.authme.security.crypts.description.Usage;
+
+@Recommendation(Usage.RECOMMENDED)
+public class Argon2 extends UnsaltedMethod {
+
+    private de.mkammerer.argon2.Argon2 argon2;
+
+    public Argon2() {
+        argon2 = Argon2Factory.create();
+    }
+
+    @Override
+    public String computeHash(String password) {
+        return argon2.hash(2, 65536, 1, password);
+    }
+}
