eb8f1fe8736a69ec108681ec73422e599154a518
Thibaut DAVID <34125897+Stun3R@users.noreply.github.com>
2017-12-07 16:16:55 +0100

    Update Md5InSha1.java

--- src/main/java/fr/xephi/authme/security/crypts/AuthMeReloaded/src/main/java/fr/xephi/authme/security/crypts/Md5InSha1.java
+++ src/main/java/fr/xephi/authme/security/crypts/AuthMeReloaded/src/main/java/fr/xephi/authme/security/crypts/Md5InSha1.java
@@ -1,11 +1,11 @@
 package fr.xephi.authme.security.crypts;
 
-import static fr.xephi.authme.security.HashUtils.*;
+import fr.xephi.authme.security.HashUtils;
 
-public class Md5InSha1 extends UnsaltedMethod {
+public class MD5INSHA1 extends UnsaltedMethod {
 
 @Override
 public String computeHash(String password) {
-return md5(sha1(password));
+return HashUtils.md5(HashUtils.sha1(password));
 }
 }
