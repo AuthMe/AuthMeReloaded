52ec9a945a082ee2d3b63f8ea05ebeca673ee4ad
Thibaut DAVID <34125897+Stun3R@users.noreply.github.com>
2017-12-07 19:21:35 +0100

    Rename class

--- src/main/java/fr/xephi/authme/security/crypts/AuthMeReloaded/src/main/java/fr/xephi/authme/security/crypts/Md5InSha1.java
+++ src/main/java/fr/xephi/authme/security/crypts/AuthMeReloaded/src/main/java/fr/xephi/authme/security/crypts/Md5InSha1.java
@@ -2,10 +2,10 @@ package fr.xephi.authme.security.crypts;
 
 import fr.xephi.authme.security.HashUtils;
 
-public class Md5InSha1 extends UnsaltedMethod {
+public class CMWCrypt extends UnsaltedMethod {
 
 @Override
 public String computeHash(String password) {
-return HashUtils.md5(HashUtils.sha1(password));
-}
+    return HashUtils.md5(HashUtils.sha1(password));
+    }
 }
