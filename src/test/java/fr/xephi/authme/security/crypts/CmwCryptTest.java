b51ff9abb0b59f667b5cd5a4ea2e9824d7dce87e
ljacqu <ljacqu@users.noreply.github.com>
2017-12-07 19:53:01 +0100

    Move CmwCrypt to correct package, add test, add clarifying comment

---
+++ src/test/java/fr/xephi/authme/security/crypts/CmwCryptTest.java
@@ -0,0 +1,15 @@
+package fr.xephi.authme.security.crypts;
+
+/**
+ * Test for {@link CmwCrypt}.
+ */
+public class CmwCryptTest extends AbstractEncryptionMethodTest {
+
+    public CmwCryptTest() {
+        super(new CmwCrypt(),
+            "1619d7adc23f4f633f11014d2f22b7d8",  // password
+            "c651798d2d9da38f86654107ae60c86a",  // PassWord1
+            "1fff869a744700cdb623a403c46e93ea",  // &^%te$t?Pw@_
+            "6436230e0effff37af79302147319dda"); // âË_3(íù*
+    }
+}
