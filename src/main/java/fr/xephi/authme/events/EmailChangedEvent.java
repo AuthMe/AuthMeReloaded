6e4bfaa266a632f28e9aabefd031cbbaeb60ca7c
toxuin <otstoypuksa@ya.ru>
2018-04-02 17:48:28 -0600

    Documented the EmailChangedEvent

--- src/main/java/fr/xephi/authme/events/EmailChangedEvent.java
+++ src/main/java/fr/xephi/authme/events/EmailChangedEvent.java
@@ -2,10 +2,13 @@ package fr.xephi.authme.events;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import javax.annotation.Nullable;
+
 /**
- * This event is called when a player changes his email address.
+ * This event is called when a player adds or changes his email address.
  */
 public class EmailChangedEvent extends CustomEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -14,7 +17,16 @@ public class EmailChangedEvent extends CustomEvent implements Cancellable {
     private final String newEmail;
     private boolean isCancelled;
 
-    public EmailChangedEvent(Player player, String oldEmail, String newEmail, boolean isAsync) {
+    /**
+     * Constructor
+     *
+     * @param player The player that changed email
+     * @param oldEmail Old email player had on file. Can be null when user adds an email
+     * @param newEmail New email that player tries to set. In case of adding email, this will contain
+     *                the email is trying to set.
+     * @param isAsync should this event be called asynchronously?
+     */
+    public EmailChangedEvent(Player player, @Nullable String oldEmail, String newEmail, boolean isAsync) {
         super(isAsync);
         this.player = player;
         this.oldEmail = oldEmail;
@@ -25,15 +37,31 @@ public class EmailChangedEvent extends CustomEvent implements Cancellable {
     public boolean isCancelled() {
         return isCancelled;
     }
-    
+
+    /**
+     * Gets the player who changes the email
+     *
+     * @return The player who changed the email
+     */
     public Player getPlayer() {
         return player;
     }
 
-    public String getOldEmail() {
+    /**
+     * Gets the old email in case user tries to change existing email.
+     *
+     * @return old email stored on file. Can be null when user never had an email and adds a new one.
+     */
+    public @Nullable String getOldEmail() {
         return this.oldEmail;
     }
 
+    /**
+     * Gets the new email.
+     *
+     * @return the email user is trying to set. If user adds email and never had one before,
+     * this is where such email can be found.
+     */
     public String getNewEmail() {
         return this.newEmail;
     }
@@ -48,6 +76,11 @@ public class EmailChangedEvent extends CustomEvent implements Cancellable {
         return handlers;
     }
 
+    /**
+     * Return the list of handlers, equivalent to {@link #getHandlers()} and required by {@link Event}.
+     *
+     * @return The list of handlers
+     */
     public static HandlerList getHandlerList() {
         return handlers;
     }
