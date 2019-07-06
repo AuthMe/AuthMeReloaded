fa46a628a3af5b55cb709a65a792bed6c04a8bd4
Mark <kiwiszijncool@gmail.com>
2019-07-06 17:36:06 +0200

    More compliance with Code Climate

--- src/main/java/fr/xephi/authme/settings/properties/NoCheatPlusFixSettings.java
+++ src/main/java/fr/xephi/authme/settings/properties/NoCheatPlusFixSettings.java
@@ -6,7 +6,7 @@ import ch.jalu.configme.Comment;
 import ch.jalu.configme.SettingsHolder;
 import ch.jalu.configme.properties.Property;
 
-public class NoCheatPlusFixSettings implements SettingsHolder {
+public final class NoCheatPlusFixSettings implements SettingsHolder {
 
     @Comment("Set this to true if players get kicked for flying when they try to login")
     public static final Property<Boolean> ENABLE_FIX = newProperty("NoCheatPlus.fixKickForFlying", false);
