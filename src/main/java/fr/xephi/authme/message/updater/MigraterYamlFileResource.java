3da21bc4f4250b3b3ac54fa8ff2fd1aad38616dc
Gabriele C. <sgdc3.mail@gmail.com>
2024-04-26 03:11:08 +0200

    Fix merge

--- src/main/java/fr/xephi/authme/message/updater/MigraterYamlFileResource.java
+++ src/main/java/fr/xephi/authme/message/updater/MigraterYamlFileResource.java
@@ -2,6 +2,7 @@ package fr.xephi.authme.message.updater;
 
 import ch.jalu.configme.resource.PropertyReader;
 import ch.jalu.configme.resource.YamlFileResource;
+import org.jetbrains.annotations.NotNull;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
@@ -19,12 +20,12 @@ public class MigraterYamlFileResource extends YamlFileResource {
     }
 
     @Override
-    public PropertyReader createReader() {
+    public @NotNull PropertyReader createReader() {
         return MessageMigraterPropertyReader.loadFromFile(getFile());
     }
 
     @Override
-    protected Yaml createNewYaml() {
+    protected @NotNull Yaml createNewYaml() {
         if (singleQuoteYaml == null) {
             DumperOptions options = new DumperOptions();
             options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
@@ -35,13 +36,5 @@ public class MigraterYamlFileResource extends YamlFileResource {
             singleQuoteYaml = new Yaml(options);
         }
         return singleQuoteYaml;
-    }
-
-    // Because we set the YAML object to put strings in single quotes, this method by default uses that YAML object
-    // and also puts all paths as single quotes. Override to just always return the same string since we know those
-    // are only message names (so never any conflicting strings like "true" or "0").
-    @Override
-    protected String escapePathElementIfNeeded(String path) {
-        return path;
     }
 }
